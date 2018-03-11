59c096e974a502b66cbece31f18aabad3c9f6ea4
jbrownrs <36203116+jbrownrs@users.noreply.github.com>
2018-03-11 00:38:23 +0000

    Update PlayerTest.py

--- PlayerTest.py
+++ PlayerTest.py
@@ -3,15 +3,15 @@ import unittest, Player
 class TestPlayer(unittest.TestCase):
 
     def test_getName(self):
-        P = Player.Player('Gareth')
-        self.assertEqual(P.getName(),'Gareth')
+        P = Player.Player('PlayerA')
+        self.assertEqual(P.getName(),'PlayerA')
 
     def test_getPosition(self):
-        P = Player.Player('Gareth')
+        P = Player.Player('PlayerA')
         self.assertEqual(P.getPosition(),1)
 
     def test_setPosition(self):
-        P = Player.Player('Gareth')
+        P = Player.Player('PlayerA')
         P.setPosition(2)
         self.assertEqual(P.getPosition(),2)
     
